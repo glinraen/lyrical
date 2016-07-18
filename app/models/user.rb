2b222a6eee7f7976f3f04a36e4a62a82528f1df3
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-07-17 22:04:58 -0700

    adds some forms.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,12 @@ class User < ActiveRecord::Base
 
 
 	validates :user_name, presence: true
-  	validates :email, presence: true, confirmation: true, uniqueness: true
-  	validates :password, presence: true, confirmation: true, on: :create
-  	validates :password, length: {minimum: 5, maximum: 120}, on: :update, allow_blank: true
+  	validates :email, presence: true, uniqueness: true
+  	validates :password, presence: true
+  	
 
   	has_secure_password
 
-  	def self.confirm(params)
-    	@user = User.find_by({email: params[:email]})
-    	@user.try(:authenticate, params[:password])
-  	end
+  	
 end
 
