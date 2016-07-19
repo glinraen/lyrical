a3366d69a8fea6c80d4465f57cd482129cf3c507
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-07-19 10:01:24 -0700

    commit to merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
 	has_many :songs
 
 	validates :user_name, presence: true
-  	validates :email, presence: true, uniqueness: true
-  	validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
   	
 
-  	has_secure_password
+  has_secure_password
   	def self.confirm(params)
     	@user = User.find_by({email: params[:email]})
     	@user.try(:authenticate, params[:password])
-  	end
+  end
 
 
   	
